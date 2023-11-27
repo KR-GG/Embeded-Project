be29f221bfe6a20a1d2e09a5b41d619201791003
KR-GG <dlgudwns0001@kau.kr>
2023-11-28 02:30:04 +0900

    11.28.02.29

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class Character:
         self.appearance = 'circle'
         self.state = None
         self.vertical_speed = 0
-        self.position = np.array([width/2 - 10, 3*height/4 - 60, width/2 + 10, 3*height/4 - 40])
+        self.position = np.array([width/2 - 10, height-40, width/2 + 10, height-20])
         self.outline = "#FFFFFF"
 
     def move(self, command = None):
@@ -93,7 +93,7 @@ class Character:
             self.position[3] = ground.position[1]
 
     def ground_check(self, ground):
-        on_ground = self.overlap(self.position, ground.position)
+        on_ground = self.on_ground(ground)
         
         if on_ground:
             self.state = 'ground'
@@ -104,9 +104,23 @@ class Character:
                 self.vertical_speed += 2.5
             else: self.state = 'fly'
     
-    def overlap(self, ch_position, gr_position):
-        return ch_position[3] >= gr_position[1] and gr_position[0] < (ch_position[0]+ch_position[2])/2 < gr_position[2]
+    def next_block_check(self, next_block):
+        on_next = self.on_ground(next_block)
+
+        if on_next:
+            self.re_positioning(next_block)
+            self.state = 'ground'
+            self.vertical_speed = 0
+            return 1
+
+    def on_ground(self, ground):
+        return ground.position[1] <= self.position[3] <= ground.position[1]+10 and ground.position[0] < (self.position[0]+self.position[2])/2 < ground.position[2]
     
+    def move_up(self, t):
+        if t:
+            self.position[1] += 5
+            self.position[3] += 5
+
 class Base:
     def __init__(self, width, height):
         self.state = 0
@@ -119,7 +133,7 @@ class Block:
         self.v = 3.5
         self.length = random.randrange(60, 90)
         start_position=random.randrange(0, width-self.length)
-        self.position = np.array([start_position, height+20, start_position+self.length, height])
+        self.position = np.array([start_position, height-20, start_position+self.length, height])
 
     def moving(self, width):
         if self.state == 'moving':
@@ -130,14 +144,28 @@ class Block:
             self.position[0] += self.v
             self.position[2] += self.v
 
+    def move_up(self, t):
+        if t:
+            self.position[1] += 5
+            self.position[3] += 5
+
 
 # 잔상이 남지 않는 코드 & 대각선 이동 가능
 my_circle = Character(joystick.width, joystick.height)
 my_base = Base(joystick.width, joystick.height)
-blocks = [Block(joystick.width, 150)]
+blocks = [] # height: 380 / 310 / 240 / 170 / 100 / 30
 my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (255, 255, 255, 100))
+stair = 0
+current_index = 2
+next_index = 3
+t = 0
 
 while True:
+    spawn_h = 380 - 70 * len(blocks)
+    while spawn_h >= 30:
+        blocks.append(Block(joystick.width, spawn_h))
+        spawn_h -= 70
+
     command = {'move': False, 'jump': False, 'up_pressed': False , 'down_pressed': False, 'left_pressed': False, 'right_pressed': False}
     
     if not joystick.button_A.value: # A pressed
@@ -159,15 +187,34 @@ while True:
         command['right_pressed'] = True
         command['move'] = True
 
-    blocks[0].moving(joystick.width)
-    my_circle.ground_check(my_base)
-    my_circle.move(command)
-    my_circle.re_positioning(my_base)
+    for block in blocks:
+        block.moving(joystick.width)
+    up = my_circle.next_block_check(blocks[next_index])
+    if up == 1:
+        stair += 1
+        blocks = blocks[1:]
+        blocks.append(Block(joystick.width, -40))
+        t += 14
+    my_circle.move_up(t)
+    for block in blocks:
+        block.move_up(t)
+    t -= 1 if t else 0
+
+    if stair == 0:
+        my_circle.ground_check(my_base)
+        my_circle.move(command)
+        my_circle.re_positioning(my_base)
+    
+    else:
+        my_circle.ground_check(blocks[current_index])
+        my_circle.move(command)
+        my_circle.re_positioning(blocks[current_index])
 
     #그리는 순서가 중요합니다. 배경을 먼저 깔고 위에 그림을 그리고 싶었는데 그림을 그려놓고 배경으로 덮는 결과로 될 수 있습니다.
     my_draw.rectangle((0, 0, joystick.width, joystick.height), fill = (255, 255, 255, 100))
-    my_draw.rectangle(tuple(my_base.position), fill = (0, 255, 0, 100))
-    my_draw.rectangle(tuple(blocks[0].position), fill = (0, 255, 0, 100))
+    if stair == 0: my_draw.rectangle(tuple(my_base.position), fill = (0, 255, 0, 100))
+    for block in blocks:
+        my_draw.rectangle(tuple(block.position), fill = (0, 255, 0, 100))
     my_draw.ellipse(tuple(my_circle.position), outline = my_circle.outline, fill = (0, 0, 0, 100))
     #좌표는 동그라미의 왼쪽 위, 오른쪽 아래 점 (x1, y1, x2, y2)
     joystick.disp.image(my_image)
